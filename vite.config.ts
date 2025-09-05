f5208112fc95e29be68f34585a5eec3eea447c52
lmontero18 <lmonterom879@gmail.com>
2025-09-05 16:58:14 -0600

    feat: add @types/node and update tsconfig for module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve("src") }],
+  },
 });
